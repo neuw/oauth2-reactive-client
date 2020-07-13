a278811127b7143933a5ca0a2517d83254e6155f
Karanbir Singh <krnbr@live.in>
2020-07-13 16:28:01 +0530

    excluding the ReactiveUserDetailsServiceAutoConfiguration from the boot. That was Not Required

--- src/main/kotlin/in/neuw/oauth2/Oauth2ReactiveClientApplication.kt
+++ src/main/kotlin/in/neuw/oauth2/Oauth2ReactiveClientApplication.kt
@@ -1,9 +1,10 @@
 package `in`.neuw.oauth2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [ReactiveUserDetailsServiceAutoConfiguration::class])
 class Oauth2ReactiveClientApplication
 
 fun main(args: Array<String>) {
